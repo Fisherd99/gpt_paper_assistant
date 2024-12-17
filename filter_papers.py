8d6ca749be7b7394faa601e51caa152066652e49
Fisherd99 <fisherd99@qq.com>
2024-12-18 00:30:04 +0800

    update calc_price and h_index_thr

--- filter_papers.py
+++ filter_papers.py
@@ -57,10 +57,10 @@ def calc_price(model, usage):
     if model == "gpt-4":
         return (0.03 * usage.prompt_tokens + 0.06 * usage.completion_tokens) / 1000.0
     if (model == "gpt-3.5-turbo") or (model == "gpt-3.5-turbo-1106"):
-        return (0.0015 * usage.prompt_tokens + 0.002 * usage.completion_tokens) / 1000.0
-    else:
-        return (0.0015 * usage.prompt_tokens + 0.002 * usage.completion_tokens) / 1000.0
-
+        return (0.001 * usage.prompt_tokens + 0.002 * usage.completion_tokens) / 1000.0
+    if model == "gpt-4o-mini":
+        return (0.00015 * usage.prompt_tokens + 0.0006 * usage.completion_tokens) / 1000.0
+# https://openai.com/api/pricing/
 
 @retry.retry(tries=3, delay=2)
 def call_chatgpt(full_prompt, openai_client, model):
